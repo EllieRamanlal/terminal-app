58040110cf64b908ff4d53b05b81490b7d877882
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-23 16:34:42 +1000

    adds main app menu

--- main.rb
+++ main.rb
@@ -6,9 +6,17 @@ prompt = TTY::Prompt.new
 def greeting
     puts "Hello!"
     puts "Welcome to HolidayHelper!"
+    puts " "
 end
 
-
+def menu(prompt)
+    choices = [
+    {name: "Find a destination for my next holiday", value: 1},
+    {name: "View previous holiday recommendations", value: 2},
+    {name: "Exit", value: 3}
+    ]
+    prompt.select("What would you like to do today?", choices)
+end
 
 
 
@@ -22,3 +30,4 @@ system 'clear'
 
 greeting
 
+menu(prompt)
