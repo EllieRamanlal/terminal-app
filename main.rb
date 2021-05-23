f118dbb91d3686823ad9be67db3d349c90885969
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-23 16:32:07 +1000

    adds welcome greeting

---
+++ main.rb
@@ -0,0 +1,24 @@
+
+require "tty-prompt"
+
+prompt = TTY::Prompt.new
+
+def greeting
+    puts "Hello!"
+    puts "Welcome to HolidayHelper!"
+end
+
+
+
+
+
+
+
+#-------------------------------------------------------
+# main body
+#-------------------------------------------------------
+
+system 'clear'
+
+greeting
+
