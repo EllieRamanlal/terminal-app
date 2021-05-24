b463565761c254bfc6714fe89a856ea26c815336
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-25 02:19:33 +1000

    adds question prompts for quiz

--- quiz.rb
+++ quiz.rb
@@ -21,6 +21,25 @@ class Quiz
         puts "\nAll you need to do is answer a couple of questions - and we'll find the perfect destination for you!"
         sleep(4)
 
+        prompt = TTY::Prompt.new(active_color: :cyan)
+
+
+        choices = [
+            {name: "I want that summer sun", value: 1},
+            {name: "Get me in my winter woolies", value: 2},
+        ]
+        weather = prompt.select("\n\nFirstly, what is your preference for weather?\n", choices)
+        sleep (2)
+
+
+        choices = [
+            {name: "Beach – Time to relax", value: 1},
+            {name: "Adventure – Let’s get active", value: 2},
+            {name: "Culture – show me the history", value: 3},
+            {name: "Nightlife – time to get loose", value: 4},
+        ]
+        vibe = prompt.select("\n\nWhat kinds of activities would you like to be doing on your holiday\n", choices)
+
 
     end
 
