78debe9ede1557633423f3fbab605f89b918d259
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-25 01:59:17 +1000

    adds destination data for quiz

---
+++ quiz.rb
@@ -0,0 +1,21 @@
+class Quiz
+
+    def initialize(name)
+        @name = name
+    end
+
+    def question
+
+        hot = ["Kauai, Hawaii", "Costa Rica", "Ho Chi Minh City, Vietnam", "Barcelona, Spain"]
+        cold = ["Kvalvika Beach, Lofoten Islands, Norway", "Whistler, Canada", "kyoto, Japan", "Prague, Czech Republic"]
+
+        beach = ["Kauai, Hawaii", "Kvalvika Beach, Lofoten Islands, Norway"]
+        adventure = ["Costa Rica", "Whistler, Canada"]
+        culture = ["Ho Chi Minh City, Vietnam", "kyoto, Japan"]
+        nightlife = ["Barcelona, Spain", "Prague, Czech Republic"]
+
+    end
+
+
+
+end
