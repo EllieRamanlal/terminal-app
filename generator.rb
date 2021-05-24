15125b029ede972ccb610a03638427754d89843a
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-24 18:25:47 +1000

    adds array of destination data for random destination generator

---
+++ generator.rb
@@ -0,0 +1,37 @@
+class Random
+
+    def initialize(name)
+        @name = name
+    end
+
+    def chooser
+        locations = [
+            "Berlin, Germany",
+            "Kyoto, Japan",
+            "Florence, Italy",
+            "Whistler, Canada",
+            "Aspen, Colorado, USA",
+            "St Moritz, Switzerland",
+            "Prague, Czech Republic",
+            "Budapest,Hungary",
+            "Amsterdam, Netherlands",
+            "New York City, USA",
+            "Paris, France",
+            "London, UK",
+            "St. Barts, The Caribbean",
+            "Mykonos, Greece",
+            "Crete, Greece",
+            "San Paolo, Brazil",
+            "Cairo City, Egypt",
+            "Hoi An, Vietnam",
+            "Marrakesh, Morocco",
+            "Barcelona, Spain",
+        ]
+
+
+    end
+
+end
+
+
+
