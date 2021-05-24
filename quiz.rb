9d2ae23ee0e87f7f0917ece748be390217d313e9
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-25 02:11:43 +1000

    adds welcome message for quiz

--- quiz.rb
+++ quiz.rb
@@ -14,6 +14,14 @@ class Quiz
         culture = ["Ho Chi Minh City, Vietnam", "kyoto, Japan"]
         nightlife = ["Barcelona, Spain", "Prague, Czech Republic"]
 
+        system 'clear'
+
+        puts "Looks like you're ready to find a destination for your next holiday!"
+        sleep(3)
+        puts "\nAll you need to do is answer a couple of questions - and we'll find the perfect destination for you!"
+        sleep(4)
+
+
     end
 
 
