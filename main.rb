91cbdc64f6d5d9f1a8887ebc5904917eda031147
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-26 19:51:07 +1000

    updates loop for main program menu

--- main.rb
+++ main.rb
@@ -21,6 +21,7 @@ heading = Artii::Base.new
 
 system 'clear'
 
+# program intro and welcome
 mainheading
 plane
 greeting
@@ -31,15 +32,45 @@ quiz_instance = Quiz.new(name)
 generator_instance = Generator.new(name)
 bucketlist_instance = Bucketlist.new(name)
 
-
+# main program menu
 loop do
-  case menu(prompt)
+  case mainmenu(prompt)
+  # Quiz
   when 1
+    quiz_loop_running = true
+    wait_clear(2)
+    puts heading.asciify('Holiday Destination Quiz').colorize(:light_cyan)
+    quiz_instance.quizwelcome
     quiz_instance.questions
-  when 2
-    generator_instance.picker
-  when 3
 
+  # Random generator
+  when 2
+    generator_loop_running = true
+    wait_clear(2)
+    puts heading.asciify('Random Desintation Generator').colorize(:light_cyan)
+    generator_instance.picker 
+
+  # Bucketlist
+  when 3
+    bucketlist_loop_running = true
+    wait_clear(2)
+    puts heading.asciify('Bucketlist').colorize(:light_cyan)
+    # bucketlist menu
+    while bucketlist_loop_running
+      case bucketlistmenu(prompt)
+      when 1
+        bucketlist_instance.view
+      when 2
+        bucketlist_instance.add
+      when 3
+        bucketlist_instance.remove
+      when 4
+        bucketlist_loop_running = false 
+      end
+    end  
+
+  # Exit
   when 4
+    exit
   end
 end
