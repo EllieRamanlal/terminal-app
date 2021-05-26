a5217dd546409e62205697fa6f455c4baf9b0f9c
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-26 19:30:52 +1000

    adds method to view bucketlist

--- bucketlist.rb
+++ bucketlist.rb
@@ -5,6 +5,25 @@ class Bucketlist
         @file_path = "./lists/#{name}.txt"
         @destinationlog = []
     end
+
+    def view
+        puts "\n#{@name.capitalize}'s bucketlist of destinations:\n"
+    
+        if file_exist?
+          unless file_empty?
+            File.open(@file_path, 'r') do |file|
+              file.each_line do |line|
+                puts "#{file.lineno}. #{line}"
+              end
+            end
+          else
+            puts "\nLooks like you haven't added any destinations yet!\n"
+            
+          end
+        else
+          create
+        end 
+    end
     
 
 
