c5bdc5b693d24c3654187041af509b1ed5a538a2
EllieRamanlal <ellie.y.ramanlal@gmail.com>
2021-05-26 18:56:47 +1000

    moves main greeting to methods file

--- methods.rb
+++ methods.rb
@@ -9,8 +9,20 @@ def mainheading
     puts "    ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝".colorize(:light_cyan)
 end
 
+
 # ascii art
 
 def plane
     puts File.readlines("plane.txt")
+end
+
+
+# greeting messages
+
+def greeting
+    puts "Hi there!"
+    puts "\nWelcome to HolidayHelper - where we help you find your perfect holiday destination!"
+    sleep (2)
+    puts "\nWhat is your name?"
+    print ">  "
 end
